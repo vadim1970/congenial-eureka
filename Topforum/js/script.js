e497df8a97dcc9dedfd8ea7e37f74661857e3e62
vadim1970 <77834937+vadim1970@users.noreply.github.com>
2021-03-04 17:53:13 +0300

    Add files via upload

--- Topforum/js/script.js
+++ Topforum/js/script.js
@@ -4,7 +4,49 @@ $(document).ready(function () {
       slidesToShow: 5,
       slidesToScroll: 1,
       prevArrow: '<button type="button" class="slick-prev"><img src="icons/leftarrow_gray.png"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="icons/rightarrow_gray.png"></button>'
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/rightarrow_gray.png"></button>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1                     
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1           
+          }
+        }
+        
+        
+          ]
+
+    });
+  });
+
+  $(document).ready(function () {
+    $('.reviews_carousel').slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/leftarrow_gray.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/rightarrow_gray.png"></button>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+                    
+          }
+        },
+       
+        
+        
+          ]
 
     });
   });
