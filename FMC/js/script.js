a349fdb5fa028563ea4a7e8e5968dcfb954ab473
vadim1970 <77834937+vadim1970@users.noreply.github.com>
2021-03-24 15:48:11 +0300

    Add files via upload

---
+++ FMC/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.expert_carousel').slick({         
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/leftarrow.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/rightarrow.png"></button>',     
+    });
+  });
