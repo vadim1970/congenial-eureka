1d6b9137ba7875471e890fdcf14eaf9026291176
vadim1970 <77834937+vadim1970@users.noreply.github.com>
2021-03-25 14:17:26 +0300

    Add files via upload

--- FMC/js/script.js
+++ FMC/js/script.js
@@ -3,4 +3,26 @@ $(document).ready(function () {
       prevArrow: '<button type="button" class="slick-prev"><img src="icons/leftarrow.png"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="icons/rightarrow.png"></button>',     
     });
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+                  
+        }
+      },               
+        ]
   });
+
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu_mobile'),
+      closeElem = document.querySelector('.menu_close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
